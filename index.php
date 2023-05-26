45b0e6ceabb7f871f64c8e797ac1dab91e1c7c81
smitbhatiya <100294455+smitbhatiy@users.noreply.github.com>
2023-05-26 09:12:47 +0530

    index

---
+++ index.php
@@ -0,0 +1,356 @@
+<?php  include 'test.php' ?>
+<?php include 'head.php' ?>
+
+
+
+<body
+	class="home page-template-default page page-id-2 wp-embed-responsive theme-porto non-logged-in woocommerce-no-js woo-variation-swatches wvs-behavior-blur wvs-theme-porto-child wvs-mobile wvs-show-label wvs-tooltip yith-wcan-free yith-wapo-frontend porto-breadcrumbs-bb login-popup full blog-1 elementor-default elementor-kit-1309 elementor-page elementor-page-2">
+
+	<div class="page-wrapper">
+    
+
+
+
+
+
+		<div id="main" class="column1 wide clearfix no-breadcrumbs"><!-- main -->
+
+			<div class="container-fluid">
+				<div class="row main-content-wrap">
+
+					<!-- main content -->
+					<div class="main-content col-lg-12">
+
+
+						<div id="content" role="main">
+
+							<article class="post-2 page type-page status-publish hentry">
+
+
+								<div class="page-content">
+									<div data-elementor-type="wp-page" data-elementor-id="2"
+										class="elementor elementor-2">
+										<section
+											class="elementor-section elementor-top-section elementor-element elementor-element-0c088e2 elementor-section-boxed elementor-section-height-default elementor-section-height-default"
+											data-id="0c088e2" data-element_type="section">
+
+											<div class="elementor-container elementor-column-gap-default">
+												<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-53f02ef"
+													data-id="53f02ef" data-element_type="column">
+													<div class="elementor-widget-wrap elementor-element-populated">
+														<div class="elementor-element elementor-element-84e83ff elementor-widget elementor-widget-slider_revolution"
+															data-id="84e83ff" data-element_type="widget"
+															data-widget_type="slider_revolution.default">
+															<div class="elementor-widget-container">
+
+																<div class="wp-block-themepunch-revslider">
+																	<!-- START Slider 1 REVOLUTION SLIDER 6.6.12 --><p class="rs-p-wp-fix"></p>
+			<rs-module-wrap id="rev_slider_1_1_wrapper" data-source="gallery" style="visibility:hidden;background:transparent;padding:0;margin:0px auto;margin-top:0;margin-bottom:0;">
+				<rs-module id="rev_slider_1_1" style="" data-version="6.6.12">
+					<rs-slides style="overflow: hidden; position: absolute;">
+						<rs-slide style="position: absolute;" data-key="rs-33" data-title="Slide" data-thumb="<?php echo SITEURL;?>/assets/images/slider-1.jpg" data-in="o:0;" data-out="a:false;">
+							<img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-1.jpg" alt="" title="Designer-Tiles" width="1920" height="500" class="rev-slidebg tp-rs-img rs-lazyload" data-lazyload="<?php echo SITEURL;?>/assets/images/slider-1.jpg" data-no-retina>
+<!---->						</rs-slide>
+						<rs-slide style="position: absolute;" data-key="rs-32" data-title="Slide" data-thumb="<?php echo SITEURL;?>/assets/images/slider-2.jpg" data-in="o:0;" data-out="a:false;">
+							<img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-2.jpg" alt="" title="Metallico banner" width="1920" height="500" class="rev-slidebg tp-rs-img rs-lazyload" data-lazyload="<?php echo SITEURL;?>/assets/images/slider-2.jpg" data-no-retina>
+<!---->						</rs-slide>
+						<rs-slide style="position: absolute;" data-key="rs-34" data-title="Slide" data-thumb="<?php echo SITEURL;?>/assets/images/slider-3.jpg" data-in="o:0;" data-out="a:false;">
+							<img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-3.jpg" alt="" title="Banner" width="1920" height="500" class="rev-slidebg tp-rs-img rs-lazyload" data-lazyload="" data-no-retina>
+<!---->						</rs-slide>
+						<rs-slide style="position: absolute;" data-key="rs-37" data-title="Slide" data-thumb="<?php echo SITEURL;?>/assets/images/slider-2.jpg" data-in="o:0;" data-out="a:false;">
+							<img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-3.jpg" alt="" title="Mosaic-Tiles-8" width="1920" height="800" class="rev-slidebg tp-rs-img rs-lazyload" data-lazyload="<?php echo SITEURL;?>/assets/images/slider-3.jpg" data-no-retina>
+<!---->						</rs-slide>
+						<rs-slide style="position: absolute;" data-key="rs-8" data-title="Slide" data-thumb="<?php echo SITEURL;?>/assets/images/slider-1.jpg" data-duration="4000ms" data-anim="adpr:false;ms:654;f:center;" data-in="o:0;sx:1.1;sy:1.1;m:true;row:30;col:30;" data-out="a:false;">
+							<img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-3.jpg" alt="" title="visuliser-2" width="1920" height="711" class="rev-slidebg tp-rs-img rs-lazyload" data-lazyload="<?php echo SITEURL;?>/assets/images/slider-3.jpg" data-bg="p:center bottom;" data-panzoom="d:10000;ss:100%;se:125%;os:0px/0;" data-no-retina>
+<!--
+							--><a
+								id="slider-1-slide-8-layer-7" 
+								class="rs-layer"
+								href="<?php echo SITEURL?>decorative-tiles" target="_self"
+								data-type="text"
+								data-tsh="h:2px,1px,0px,0px;v:2px,1px,0px,0px;b:2px,1px,0px,0px;"
+								data-rsp_ch="on"
+								data-xy="x:c;xo:26px,21px,15px,9px;y:m;yo:-243px,-200px,-151px,-93px;"
+								data-text="w:normal;s:43,35,26,16;l:58,47,35,21;ls:3,2,1,0;a:center;"
+								data-dim="minh:0,none,none,none;"
+								data-padding="t:11,9,7,4;r:11,9,7,4;b:11,9,7,4;l:11,9,7,4;"
+								data-frame_0_words="d:5;"
+								data-frame_0_lines="d:5;"
+								data-frame_1="x:-48px,-39px,-29px,-17px;y:71px,58px,44px,27px;z:-200px;"
+								data-frame_1_words="d:5;"
+								data-frame_1_lines="d:5;"
+								data-frame_1_sfx="se:blocktoleft;"
+								data-frame_999="o:0;st:w;"
+								style="z-index:8;background-color:rgba(42,43,35,0.81);font-family:'Raleway';"
+							> <br /><b> </b> 
+							</a><!--
+-->						</rs-slide>
+						<rs-slide style="position: absolute;" data-key="rs-7" data-title="Slide" data-thumb="<?php echo SITEURL;?>/assets/images/slider-1.jpg" data-anim="adpr:false;f:center;" data-in="o:0;sx:1.1;sy:1.1;m:true;row:30;col:30;" data-out="a:false;">
+							<img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-2.jpgg" alt="" title="Decorative-Tiles-Collection-1" width="1920" height="500" class="rev-slidebg tp-rs-img rs-lazyload" data-lazyload="<?php echo SITEURL;?>/assets/images/slider-3.jpg" data-bg="p:center bottom;" data-panzoom="d:10000;ss:100%;se:125%;os:0px/0;" data-no-retina>
+<!--
+							--><a
+								id="slider-1-slide-7-layer-6" 
+								class="rs-layer rev-btn"
+								href="<?php echo SITEURL?>border-tiles" target="_self"
+								data-type="button"
+								data-rsp_ch="on"
+								data-xy="x:c;xo:2px,1px,0,-17px;y:m,m,b,b;yo:-65px,-53px,130px,80px;"
+								data-text="w:normal;s:20,16,12,7;l:50,41,31,19;fw:500;"
+								data-dim="minh:0px,none,none,none;"
+								data-padding="r:20,17,13,8;l:20,17,13,8;"
+								data-frame_0="x:175%;o:1;"
+								data-frame_0_mask="u:t;x:-100%;"
+								data-frame_1="e:power3.out;sp:1000;"
+								data-frame_1_mask="u:t;"
+								data-frame_999="o:0;st:w;"
+								data-frame_hover="c:#000;bgc:#fff;bor:0px,0px,0px,0px;sp:200;e:power1.inOut;"
+								style="z-index:10;background-color:#392416;font-family:'Roboto';"
+							> 
+							</a><!--
+
+							--><rs-layer
+								id="slider-1-slide-7-layer-7" 
+								data-type="text"
+								data-tsh="h:2px,1px,0px,0px;v:2px,1px,0px,0px;b:2px,1px,0px,0px;"
+								data-rsp_ch="on"
+								data-xy="x:c;xo:-6px,-4px,-3px,-1px;y:m;yo:-176px,-145px,-110px,-67px;"
+								data-text="w:normal;s:40,33,25,15;l:54,44,33,20;ls:3,2,1,0;a:center;"
+								data-dim="minh:0,none,none,none;"
+								data-padding="t:10,8,6,4;r:10,8,6,4;b:10,8,6,4;l:10,8,6,4;"
+								data-frame_0_words="d:5;"
+								data-frame_0_lines="d:5;"
+								data-frame_1="x:1px,0px,0px,0px;"
+								data-frame_1_words="d:5;"
+								data-frame_1_lines="d:5;"
+								data-frame_1_sfx="se:blocktoleft;"
+								data-frame_999="o:0;st:w;"
+								style="z-index:8;background-color:rgba(57,36,22,0.81);font-family:'Raleway';"
+							> 
+							</rs-layer><!--
+-->						</rs-slide>
+						<rs-slide style="position: absolute;" data-key="rs-5" data-title="Slide" data-thumb="<?php echo SITEURL;?>/assets/images/slider-3.jpg" data-anim="adpr:false;ms:1500;f:nodelay;" data-filter="b:2;" data-in="o:0;sx:1.5;sy:1.2;m:true;col:6;" data-out="a:false;">
+							<img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-1.jpg" alt="Slide" title="Decorative-Tiles-Collection!" width="1920" height="500" class="rev-slidebg tp-rs-img rs-lazyload" data-lazyload="<?php echo SITEURL;?>/assets/images/slider-3.jpg" data-bg="c:linear-gradient(rgba(74,74,74,1) 0%, rgba(12,12,12,1) 100%);" data-no-retina>
+<!--
+							--><rs-layer
+								id="slider-1-slide-5-layer-0" 
+								data-type="image"
+								data-rsp_ch="on"
+								data-xy="x:c;y:m;"
+								data-text="w:normal;s:20,10,7,4;l:0,13,9,6;"
+								data-dim="w:732px,390px,779px,481px;h:411px,218px,437px,269px;"
+								data-frame_1="x:10px,5px,3px,1px;y:2px,1px,0px,0px;"
+								data-frame_999="o:0;st:w;"
+								style="z-index:9;"
+							><img decoding="async" src="<?php echo SITEURL;?>/assets/images/slider-2.jpg" alt="" class="tp-rs-img rs-lazyload" width="620" height="348" data-lazyload="<?php echo SITEURL;?>/assets/images/slider-1.jpg" data-no-retina> 
+							</rs-layer><!--
+
+							--><rs-layer
+								id="slider-1-slide-5-layer-3" 
+								data-type="text"
+								data-tsh="h:2px,1px,0px,0px;v:2px,1px,0px,0px;b:2px,1px,0px,0px;"
+								data-rsp_ch="on"
+								data-xy="x:c;xo:19px,10px,-5px,-2px;yo:-4px,-2px,69px,103px;"
+								data-text="w:normal;s:74,39,29,41;l:100,53,40,58;ls:5,2,1,0;"
+								data-dim="minh:0,none,none,none;"
+								data-frame_0_words="d:5;"
+								data-frame_0_lines="d:5;"
+								data-frame_1_words="d:5;"
+								data-frame_1_lines="d:5;"
+								data-frame_1_sfx="se:blocktoleft;"
+								data-frame_999="o:0;st:w;"
+								style="z-index:6;font-family:'Shadows Into Light';"
+							> 
+							</rs-layer><!--
+
+							--><rs-layer
+								id="slider-1-slide-5-layer-4" 
+								data-type="button"
+								data-rsp_ch="on"
+								data-xy="x:c;xo:-3px,-1px,8px,0;yo:80px,42px,202px,167px;"
+								data-text="w:nowrap;s:40,21,15,31;l:50,26,19,38;fw:300;"
+								data-dim="w:182px,96px,72px,152px;"
+								data-frame_999="o:0;st:w;"
+								style="z-index:7;font-family:'Montserrat';"
+							>  
+							</rs-layer><!--
+
+							--><a
+								id="slider-1-slide-5-layer-5" 
+								class="rs-layer rev-btn"
+								href="<?php echo SITEURL?>Quartz-sink" target="_self"
+								data-type="button"
+								data-rsp_ch="on"
+								data-xy="x:c;xo:0,0,0,-17px;y:t,t,b,b;yo:780px,415px,130px,80px;"
+								data-text="w:normal;s:20,10,7,4;l:50,26,19,11;fw:500;"
+								data-dim="minh:0px,none,none,none;"
+								data-padding="r:20,11,8,5;l:20,11,8,5;"
+								data-frame_0="x:175%;o:1;"
+								data-frame_0_mask="u:t;x:-100%;"
+								data-frame_1="e:power3.out;sp:1000;"
+								data-frame_1_mask="u:t;"
+								data-frame_999="o:0;st:w;"
+								data-frame_hover="c:#000;bgc:#fff;bor:0px,0px,0px,0px;sp:200;e:power1.inOut;"
+								style="z-index:8;background-color:rgba(0,0,0,1);font-family:'Roboto';"
+							> 
+							</a><!--
+-->						</rs-slide>
+					</rs-slides>
+					<rs-static-layers><!--
+					--></rs-static-layers>
+				</rs-module>
+				
+			</rs-module-wrap>
+			<!-- END REVOLUTION SLIDER -->
+																</div>
+
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</section>
+
+                                        <div class="cards-container">
+
+
+        <div class="woocommerce columns-3"><ul class="products products-container grid category-pos-outside category-text-center category-color-dark pcols-lg-3 pcols-md-3 pcols-xs-2 pcols-ls-3 pwidth-lg-3 pwidth-md-3 pwidth-xs-2 pwidth-ls-1 is-shortcode" data-product_layout="product-awq_onimage">
+
+<li class="product-category product-col">
+		<a href="<?php echo SITEURL?>decorative-tiles" aria-label="category">
+		<div class="thumb-info  align-center">
+									<div class="thumb-info-wrapper tf-none">
+					<img decoding="async" loading="lazy" src="<?php echo SITEURL?>assets/images/poster.jpg" alt="Decorative Tiles" width="768" height="432" >				</div>
+													<div class="thumb-info-wrap">
+					<div class="thumb-info-title">
+																<h3 class="sub-title thumb-info-inner">Decorative Tiles</h3>
+																		
+																					<span class="btn">View All Products<i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+											</div>
+				</div>
+					</div>
+	</a>
+
+	
+	<div class="woocommerce columns-1 "></div></li>
+
+<li class="product-category product-col">
+		<a href="<?php echo SITEURL?>border-tiles" aria-label="category">
+		<div class="thumb-info  align-center">
+									<div class="thumb-info-wrapper tf-none">
+					<img decoding="async" loading="lazy" src="<?php echo SITEURL?>assets/images/border.jpg" alt="Floor Tiles" width="768" height="432" >				</div>
+													<div class="thumb-info-wrap">
+					<div class="thumb-info-title">
+																<h3 class="sub-title thumb-info-inner">Border Tiles</h3>
+																		
+																					<span class="btn">View All Products<i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+											</div>
+				</div>
+					</div>
+	</a>
+
+	
+	<div class="woocommerce columns-1 "></div></li>
+
+<li class="product-category product-col">
+		<a href="<?php echo SITEURL?>gold-silver-highliters" aria-label="category">
+		<div class="thumb-info  align-center">
+									<div class="thumb-info-wrapper tf-none">
+					<img decoding="async" loading="lazy" src="<?php echo SITEURL?>assets/images/img.jpg" alt="Wall Tiles"  >				</div>
+													<div class="thumb-info-wrap">
+					<div class="thumb-info-title">
+																<h3 class="sub-title thumb-info-inner">Gold Silver Highliters</h3>
+																		
+																					<span class="btn">View All Products<i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+											</div>
+				</div>
+					</div>
+	</a>
+
+	
+	<div class="woocommerce columns-1 "></div></li>
+
+<li class="product-category product-col">
+		<a href="<?php echo SITEURL?>Quartz-sink" aria-label="category">
+		<div class="thumb-info  align-center">
+									<div class="thumb-info-wrapper tf-none">
+					<img decoding="async" loading="lazy" src="<?php echo SITEURL?>assets/images/sink_image.jpg" alt="Kitchen Tiles"></div>
+													<div class="thumb-info-wrap">
+					<div class="thumb-info-title">
+																<h3 class="sub-title thumb-info-inner">Quartz Sink</h3>
+																		
+																					<span class="btn">View All Products<i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+											</div>
+				</div>
+					</div>
+	</a>
+
+	
+	<div class="woocommerce columns-1 "></div></li>
+
+<li class="product-category product-col">
+		<a href="<?php echo SITEURL?>Quartz-sink" aria-label="category">
+		<div class="thumb-info  align-center">
+									<div class="thumb-info-wrapper tf-none">
+					<img decoding="async" loading="lazy" src="<?php echo SITEURL?>assets/images/vanity.jpg" alt="Elevation Tiles">				</div>
+													<div class="thumb-info-wrap">
+					<div class="thumb-info-title">
+																<h3 class="sub-title thumb-info-inner">Vanity Set</h3>
+																		
+																					<span class="btn">View All Products<i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+											</div>
+				</div>
+					</div>
+	</a>
+
+	
+	<div class="woocommerce columns-1 "></div></li>
+
+<li class="product-category product-col">
+		<a href="<?php echo SITEURL?>slab-tiles" aria-label="category">
+		<div class="thumb-info  align-center">
+									<div class="thumb-info-wrapper tf-none">
+					<img decoding="async" loading="lazy" src="<?php echo SITEURL?>assets/images/SPACER.jpg" alt="Outdoor Tiles"></div>
+					<div class="thumb-info-title">
+																<h3 class="sub-title thumb-info-inner">Tiles Spacer</h3>
+																		
+																					<span class="btn">View All Products<i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+											</div>
+				</div>
+					</div>
+	</a>
+
+	
+	<div class="woocommerce columns-1 "></div></li>
+</ul>
+</div>
+
+									
+									</div>
+								</div>
+							</article>
+
+						</div>
+
+
+
+					</div><!-- end main content -->
+
+
+
+				</div>
+			</div>
+
+
+
+		</div><!-- end main -->
+
+
+	
+
+        <?php include 'footer.php'; ?>
+
+
+	</div><!-- end wrapper -->
+</body>
+
+</html>
